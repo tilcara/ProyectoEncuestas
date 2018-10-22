aef19f01bc8883fd76083d090651feda6505cd1d
tilcara <enriquesegovianoandroid@gmail.com>
2018-10-22 19:35:26 -0300

    Vistas basadas en funciones

--- ProyectoEncuestas/encuestas/urls.py
+++ ProyectoEncuestas/encuestas/urls.py
@@ -2,6 +2,18 @@ from django.urls import path
 
 from . import views
 
+app_name='encuestas'
+
 urlpatterns = [
+    # ej: /encuestas/
     path('', views.index, name='index'),
+
+    # ej: /encuestas/5/
+    path('<int:pregunta_id>/', views.detalle, name='detalle'),
+    
+    # ej: /encuestas/5/resultados/
+    path('<int:pregunta_id>/resultados/', views.resultados, name='resultados'),
+    
+    # ej: /encuestas/5/votar/
+    path('<int:pregunta_id>/votar/', views.votar, name='votar'),
 ]
--- ProyectoEncuestas/encuestas/views.py
+++ ProyectoEncuestas/encuestas/views.py
@@ -1,5 +1,36 @@
-from django.http import HttpResponse
+
+
+from django.http import HttpResponse, HttpResponseRedirect
+from django.shortcuts import render, get_object_or_404
+from django.urls import reverse
+
+from .models import Pregunta, Opcion
 
 
 def index(request):
-    return HttpResponse("Hola mundo. Este es el index de la app encuestas.")
+	
+	ultimas_preguntas=Pregunta.objects.order_by('-fecha_publicacion')[:5]
+	contexto={'ultimas_preguntas':ultimas_preguntas,}
+	return render(request,'encuestas/index.html',contexto)	
+	
+def detalle(request, pregunta_id):
+
+	pregunta=get_object_or_404(Pregunta,pk=pregunta_id)
+	return render(request,'encuestas/detalle.html',{'pregunta':pregunta})
+
+def resultados(request, pregunta_id):
+
+	pregunta=get_object_or_404(Pregunta,pk=pregunta_id)
+	return render(request,'encuestas/resultados.html',{'pregunta':pregunta})
+
+def votar(request, pregunta_id):
+
+	pregunta=get_object_or_404(Pregunta,pk=pregunta_id)
+	try:
+		opcion_elegida=pregunta.opcion_set.get(pk=request.POST['opcion'])
+	except (KeyError, Opcion.DoesNotExist):
+		return render(request,'encuestas/detalle.html',{'pregunta':pregunta,'error_message':"No elegiste una opcion",})	
+	else:
+		opcion_elegida.votos+=1
+		opcion_elegida.save()
+		return HttpResponseRedirect(reverse('encuestas:resultados',args=(pregunta.id,)))
